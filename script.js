bb9c1e078b70d391f030eba6a56ff719ab1da939
Steve Kindt <stevemkindt@gmail.com>
2020-01-08 22:06:08 -0600

    pseudocode css and js

---
+++ script.js
@@ -0,0 +1,4 @@
+// Use confirm (Y/N) or prompt (if necessary) to allow user to choose character types //
+// Ensure that at least one character type is chosen with a check (if/else where else is null?) and alert user to select at least one type //
+// List selectable characters in a string or array? //
+// Maybe password output is defaulted to "Your Secure Password" as text and it's replaced with the password generated upon event? Need to use preventDefault in that case? //

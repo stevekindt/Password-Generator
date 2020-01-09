830fd69ed391101042f5320335449a4ac667f7ff
Steve Kindt <stevemkindt@gmail.com>
2020-01-09 06:30:20 -0600

    User Prompts

--- script.js
+++ script.js
@@ -1,4 +1,32 @@
-// Use confirm (Y/N) or prompt (if necessary) to allow user to choose character types //
-// Ensure that at least one character type is chosen with a check (if/else where else is null?) and alert user to select at least one type //
-// List selectable characters in a string or array? //
-// Maybe password output is defaulted to "Your Secure Password" as text and it's replaced with the password generated upon event? Need to use preventDefault in that case? //
+// Put DOM elments here
+
+// Use confirm (Y/N) or prompt (if necessary) to allow user to choose character types
+
+let includeSpecials = confirm("Would you like to include special characters in your password?");
+
+alert(includeSpecials);
+
+let includeNumbers = confirm("Would you like to include numbers in your password?");
+
+alert(includeNumbers);
+
+let includeLowers = confirm("Would you like to include lowercase letters in your password?");
+
+alert(includeLowers);
+
+let includeUppers = confirm("Would you like to include uppercase letters in your password?");
+
+alert(includeUppers);
+
+
+
+// Random generator functions
+
+function getRandomNumber() {
+    const numbers = 0123456789;
+    return numbers[Math.floor(Math.random() * numbers.length)];
+}
+
+// Ensure that at least one character type is chosen with a check (if/else where else is null?) and alert user to select at least one type
+// List selectable characters in a string or array?
+// Maybe password output is defaulted to "Your Secure Password" as text and it's replaced with the password generated upon event? Need to use preventDefault in that case?

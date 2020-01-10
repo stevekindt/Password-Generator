f5a2190fcac600fad7084756cd943e4ea42b6264
Steve Kindt <stevemkindt@gmail.com>
2020-01-09 19:46:40 -0600

    clipboard function

--- script.js
+++ script.js
@@ -38,6 +38,12 @@ function getRandomUpper() {
     return uppers[Math.floor(Math.random() * uppers.length)];
 }
 
+function copy() {
+    let textarea = document.getElementById("result");
+    textarea.select();
+    document.execCommand("copy");
+  }
+
 // Ensure that at least one character type is chosen with a check (if/else where else is null?) and alert user to select at least one type
 // List selectable characters in a string or array?
 // Maybe password output is defaulted to "Your Secure Password" as text and it's replaced with the password generated upon event? Need to use preventDefault in that case?
